e37d5d8aeb4cd050b53e619bda6aaac515d408a1
AlejandroMontalvo <alejandrormont@gmail.com>
2024-05-12 22:37:22 -0400

    navbar styling

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.onload = function () {
+  const loadingAnimation = document.getElementById("loading-animation");
+  loadingAnimation.innerHTML =
+    '<object type="image/svg+xml" data="./imgs/ring-resize.svg"></object>'; // Show loading animation
+
   // Fetch data from JSON file
   fetch("data.json")
     .then((response) => response.json())
@@ -8,9 +12,15 @@ window.onload = function () {
         const cardElement = document.createElement("a");
         cardElement.classList.add("card");
         cardElement.href = card.link; // Set the href to the card's link
+        cardElement.target = "_blank";
 
         const imageElement = document.createElement("img");
         imageElement.src = card.image;
+        imageElement.onload = () => {
+          // Hide loading animation when image is fully loaded
+          loadingAnimation.style.display = "none";
+          cardElement.style.display = "block";
+        };
         cardElement.appendChild(imageElement);
 
         const titleElement = document.createElement("h2");
