023dde7fb1637ccbf8f53d451d339f11f72cbc60
AlejandroMontalvo <alejandrormont@gmail.com>
2024-08-15 22:14:51 -0400

    refactoring

--- script.js
+++ script.js
@@ -1,126 +1,111 @@
 window.onload = function () {
+  // Elements
   const loadingAnimation = document.getElementById("loading-animation");
-  loadingAnimation.innerHTML =
-    '<object type="image/svg+xml" data="./imgs/ring-resize.svg"></object>'; // Show loading animation
+  const aboutSection = document.getElementById("about");
+  const hamburgerMenu = document.getElementById("hamburger-menu");
+  const navLinks = document.getElementById("nav-links");
+  const navbar = document.getElementById("navbar");
+  const selectedWorks = document.getElementById("selected-works");
+  const recentWorks = document.getElementById("recent-works");
 
-  // Function to hide loading animation
+  // Show loading animation
+  loadingAnimation.innerHTML =
+    '<object type="image/svg+xml" data="./imgs/ring-resize.svg"></object>';
+
+  // Hide loading animation
   const hideLoadingAnimation = () => {
-    setTimeout(() => {
-      loadingAnimation.style.transition = "transform 1s ease-out";
-      loadingAnimation.style.transform = "translateY(-100vh)";
-    }, 1000).then(() => (loadingAnimation.style.display = "none"));
+    loadingAnimation.style.transition = "transform 1s ease-out";
+    loadingAnimation.style.transform = "translateY(-100vh)";
+    loadingAnimation.addEventListener("transitionend", () => {
+      loadingAnimation.style.display = "none";
+    });
   };
 
-  // Function to check if the background image of the 'about' section has loaded
+  // Check if background image has loaded
   const checkBackgroundImageLoaded = (callback) => {
-    const aboutSection = document.getElementById("about");
     const bgImage = new Image();
     const bgUrl = window
       .getComputedStyle(aboutSection)
       .backgroundImage.slice(5, -2);
-
-    console.log(bgUrl);
-
     bgImage.onload = callback;
     bgImage.src = bgUrl;
   };
 
+  // Toggle navLinks visibility and navbar background color
+  const toggleNavLinks = () => {
+    navLinks.classList.toggle("show");
+    navbar.style.backgroundColor = navLinks.classList.contains("show")
+      ? "#9abed8"
+      : "transparent";
+  };
+
+  hamburgerMenu.addEventListener("click", toggleNavLinks);
+
+  // Hide navLinks when a link is clicked
+  navLinks.addEventListener("click", (event) => {
+    if (event.target.tagName === "A" && navLinks.classList.contains("show")) {
+      toggleNavLinks();
+    }
+  });
+
   // Fetch data and update the page
+  const createCardElement = (work, isSelectedWork) => {
+    const cardElement = document.createElement("a");
+    cardElement.classList.add("card");
+    if (isSelectedWork) {
+      cardElement.classList.add("column");
+    }
+    cardElement.href = work.link;
+    cardElement.target = "_blank";
+
+    const imageElement = document.createElement("img");
+    imageElement.src = work.image;
+    cardElement.appendChild(imageElement);
+
+    const content = document.createElement("div");
+    content.classList.add("card-content");
+    cardElement.appendChild(content);
+
+    if (work.title) {
+      const titleElement = document.createElement("h2");
+      titleElement.textContent = work.title;
+      content.appendChild(titleElement);
+    }
+
+    if (work.description) {
+      const descriptionElement = document.createElement("p");
+      descriptionElement.textContent = work.description;
+      content.appendChild(descriptionElement);
+    }
+
+    const tagContainer = document.createElement("div");
+    tagContainer.classList.add("tag-container");
+    if (work.tags) {
+      work.tags.forEach((tag) => {
+        const tagElement = document.createElement("div");
+        tagElement.classList.add("tag");
+        tagElement.textContent = tag;
+        tagContainer.appendChild(tagElement);
+      });
+    }
+    content.appendChild(tagContainer);
+
+    return cardElement;
+  };
+
   fetch("data.json")
     .then((response) => response.json())
     .then((data) => {
-      const selectedWorks = document.getElementById("selected-works");
       data["selected-works"].forEach((work) => {
-        const cardElement = document.createElement("a");
-        cardElement.classList.add("card");
-        cardElement.href = work.link;
-        cardElement.target = "_blank";
-
-        const imageElement = document.createElement("img");
-        imageElement.src = work.image;
-
-        cardElement.appendChild(imageElement);
-
-        const content = document.createElement("div");
-        content.classList.add("card-content");
-        cardElement.appendChild(content);
-
-        const titleElement = document.createElement("h2");
-        titleElement.textContent = work.title;
-        content.appendChild(titleElement);
-
-        if (work.description) {
-          const descriptionElement = document.createElement("p");
-          descriptionElement.textContent = work.description;
-          content.appendChild(descriptionElement);
-        }
-
-        const tagContainer = document.createElement("div");
-        tagContainer.classList.add("tag-container");
-        if (work.tags) {
-          work.tags.forEach((tag) => {
-            const tagElement = document.createElement("div");
-            tagElement.classList.add("tag");
-            tagElement.textContent = tag;
-            tagContainer.appendChild(tagElement);
-          });
-        }
-        content.appendChild(tagContainer);
-        selectedWorks.appendChild(cardElement);
+        selectedWorks.appendChild(createCardElement(work, true));
       });
-
-      const recentWorks = document.getElementById("recent-works");
       data["recent-works"].forEach((work) => {
-        const cardElement = document.createElement("a");
-        cardElement.classList.add("card");
-        cardElement.classList.add("rows");
-        cardElement.href = work.link;
-        cardElement.target = "_blank";
-
-        const imageElement = document.createElement("img");
-        imageElement.src = work.image;
-        cardElement.appendChild(imageElement);
-
-        const content = document.createElement("div");
-        content.classList.add("card-content");
-        cardElement.appendChild(content);
-
-        if (work.title) {
-          const titleElement = document.createElement("h2");
-          titleElement.textContent = work.title;
-          content.appendChild(titleElement);
-        }
-
-        if (work.description) {
-          const descriptionElement = document.createElement("p");
-          descriptionElement.textContent = work.description;
-          content.appendChild(descriptionElement);
-        }
-
-        const tagContainer = document.createElement("div");
-        tagContainer.classList.add("tag-container");
-        if (work.tags) {
-          work.tags.forEach((tag) => {
-            const tagElement = document.createElement("div");
-            tagElement.classList.add("tag");
-            tagElement.textContent = tag;
-            tagContainer.appendChild(tagElement);
-          });
-        }
-        content.appendChild(tagContainer);
-
-        recentWorks.appendChild(cardElement);
+        recentWorks.appendChild(createCardElement(work, false));
       });
     })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
-    });
+    .catch((error) => console.error("Error fetching data:", error));
 
-  // Check if the background image of the 'about' section has loaded, then hide the loading animation
-  checkBackgroundImageLoaded(hideLoadingAnimation);
-
-  // Make the navbar turn blue when you scroll down 50px
-  const navbar = document.getElementById("navbar");
+  // Navbar scroll behavior
   window.onscroll = () => {
     if (window.scrollY > 120) {
       navbar.classList.add("navbar-scroll");
@@ -129,10 +114,10 @@ window.onload = function () {
     }
   };
 
-  // Make it so the navbar link being underlined corresponds to the current section on the page
+  // Update navbar link based on scroll position
   window.addEventListener("scroll", () => {
     const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("#nav-links a");
+    const navLinksArray = Array.from(navLinks.querySelectorAll("a"));
 
     sections.forEach((section) => {
       const sectionTop = section.offsetTop;
@@ -145,13 +130,16 @@ window.onload = function () {
         scrollPosition >= sectionTop &&
         scrollPosition < sectionTop + sectionHeight
       ) {
-        navLinks.forEach((link) => {
-          link.classList.remove("active");
-          if (link.getAttribute("href").endsWith(`#${sectionId}`)) {
-            link.classList.add("active");
-          }
+        navLinksArray.forEach((link) => {
+          link.classList.toggle(
+            "active",
+            link.getAttribute("href").endsWith(`#${sectionId}`)
+          );
         });
       }
     });
   });
+
+  // Check if the background image of the 'about' section has loaded, then hide the loading animation
+  checkBackgroundImageLoaded(hideLoadingAnimation);
 };
